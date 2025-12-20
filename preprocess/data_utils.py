2a35efa0298d0ef33272b247ccb819237aaf35e6
Manajit Das <59758360+manajit-das@users.noreply.github.com>
2025-12-20 11:45:33 +0530

    Remove mydataset class from data_utils.py
    Removed the mydataset class and its methods for handling SMILES data.

--- preprocess/data_utils.py
+++ preprocess/data_utils.py
@@ -14,51 +14,6 @@ SMI_REGEX_PATTERN = (
     r"%[0-9]{2}|[0-9])"
 )
 
-
-class mydataset(Dataset):
-  def __init__(self, input_csv_path):
-    df = pd.read_csv(input_csv_path)
-    self.src_smiles = df['src_smiles'].tolist()
-    self.tgt_smiles = df['tgt_smiles'].tolist()
-    self.vocab = self.create_vocab(self.src_smiles+self.tgt_smiles)
-    self.vocab_list = sorted(self.vocab)
-    #add special chracters
-    self.vocab_list = ['PAD', 'BOS', 'EOS', '<UNK>'] + self.vocab_list
-
-    self.char_to_int = {tok: i for i, tok in enumerate(self.vocab_list)}
-    self.int_to_char = {i: tok for i, tok in enumerate(self.vocab_list)}
-    self.vocab_size = len(self.vocab_list)
-
-
-  @staticmethod
-  def create_vocab(smiles_list):
-    vocab = set()
-    for smiles in smiles_list:
-        tokens = re.findall(SMI_REGEX_PATTERN, smiles)
-        vocab.update(tokens)        # set.update works with a list
-    return vocab
-  
-  def tokenize_smiles(self, smiles):
-    tokens = re.findall(SMI_REGEX_PATTERN, smiles)
-    token_id = [self.char_to_int.get(t, self.char_to_int['<UNK>']) for t in tokens]
-    return token_id
-
-  def __len__(self):
-    return len(self.src_smiles)
-
-  def __getitem__(self, idx):
-    src_smiles = self.src_smiles[idx]
-    tgt_smiles = self.tgt_smiles[idx]
-    #tokenize the smiles
-    src_tokens = self.tokenize_smiles(src_smiles)
-    tgt_tokens = [self.char_to_int['BOS']] + self.tokenize_smiles(tgt_smiles) + [self.char_to_int['EOS']]
-    src_seq_len = len(src_tokens)
-    tgt_seq_len = len(tgt_tokens)
-    return src_tokens, tgt_tokens, src_seq_len, tgt_seq_len
-
-
-import torch
-
 class TokenizedSeq2SeqDataset(Dataset):
     def __init__(self, pt_path):
         data = torch.load(pt_path, map_location="cpu")
